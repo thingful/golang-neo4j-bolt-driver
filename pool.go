229ce2bacc633c41b9e6704579bfb6807c95aee4
Sam Mulube <sam@thingful.net>
2017-09-26 20:44:27 +0100

    Try adding timeout to prevent blocking waiting on pool forever

--- pool.go
+++ pool.go
@@ -3,6 +3,7 @@ package neo4jbolt
 import (
 	"errors"
 	"sync"
+	"time"
 
 	"github.com/thingful/golang-neo4j-bolt-driver/log"
 )
@@ -16,9 +17,6 @@ type Pool interface {
 	// Close closes the pool and all its connections. After Close() the pool is
 	// no longer usable.
 	Close()
-
-	// Len returns the current number of connections in the pool.
-	Len() int
 }
 
 type connPool struct {
@@ -61,6 +59,8 @@ func (c *connPool) Get() (Conn, error) {
 		if conn == nil {
 			return nil, ErrClosed
 		}
+	case <-time.After(c.driver.poolTimeout):
+		return nil, ErrPoolTimeout
 	default:
 		conn, err = newBoltConn(c.driver, c)
 		if err != nil {
@@ -78,7 +78,7 @@ func (c *connPool) put(conn *boltConn) error {
 		return errors.New("connection is nil, rejecting")
 	}
 
-	log.Info("*********** Putting conn back into pool")
+	log.Info("Putting conn back into pool")
 
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -105,76 +105,3 @@ func (c *connPool) getConns() chan *boltConn {
 	c.mu.Unlock()
 	return conns
 }
-
-func (c *connPool) Len() int { return len(c.getConns()) }
-
-// DriverPool is a driver allowing connection to Neo4j with support for connection pooling
-// The driver allows you to open a new connection to Neo4j
-//
-// Driver objects should be THREAD SAFE, so you can use them
-// to open connections in multiple threads.  The connection objects
-// themselves, and any prepared statements/transactions within ARE NOT
-// THREAD SAFE.
-//type DriverPool interface {
-//	// OpenPool opens a Neo-specific connection.
-//	OpenPool() (Conn, error)
-//	reclaim(*boltConn) error
-//}
-//
-//type boltDriverPool struct {
-//	connStr  string
-//	maxConns int
-//	pool     chan *boltConn
-//}
-
-// NewDriverPool creates a new Driver object with connection pooling
-//func NewDriverPool(connStr string, max int) (DriverPool, error) {
-//	d := &boltDriverPool{
-//		connStr:  connStr,
-//		maxConns: max,
-//		pool:     make(chan *boltConn, max),
-//	}
-//
-//	for i := 0; i < max; i++ {
-//		conn, err := newdBoltConn(connStr, d)
-//		if err != nil {
-//			return nil, err
-//		}
-//
-//		d.pool <- conn
-//	}
-//
-//	return d, nil
-//}
-//
-//// OpenNeo opens a new Bolt connection to the Neo4J database.
-//func (d *boltDriverPool) OpenPool() (Conn, error) {
-//	conn := <-d.pool
-//	if conn.conn == nil {
-//		if err := conn.initialize(); err != nil {
-//			return nil, err
-//		}
-//	}
-//	return conn, nil
-//}
-//
-//func (d *boltDriverPool) reclaim(conn *boltConn) error {
-//	var newConn *boltConn
-//	var err error
-//	if conn.connErr != nil || conn.closed {
-//		newConn, err = newPooledBoltConn(d.connStr, d)
-//		if err != nil {
-//			return err
-//		}
-//	} else {
-//		// sneakily swap out connection so a reference to
-//		// it isn't held on to
-//		newConn = &boltConn{}
-//		*newConn = *conn
-//	}
-//
-//	d.pool <- newConn
-//	conn = nil
-//
-//	return nil
-//}
